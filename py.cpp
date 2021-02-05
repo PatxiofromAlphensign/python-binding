03a156c5e3c879d01b581c2e3b86fc00c386abda
PatxiofromAlphensign <aissypewdiepie@gmail.com>
2021-02-05 22:00:14 +0530

    initial

---
+++ py.cpp
@@ -0,0 +1,50 @@
+#include <Python.h> 
+#include <stdlib.h> 
+#include <iostream> 
+
+static int pyobj(const char *cmd) {
+    return system(cmd);
+    }
+
+static PyObject *add(PyObject *self, PyObject *args)  {
+    (void *)pyobj("ls -l");
+    int i;
+    while (i<10){
+        i++;
+        }
+
+    if (!PyArg_ParseTuple(args, "s", &i)) {return NULL;}
+    return PyLong_FromLong(i);
+
+}
+static PyObject *spam(PyObject *self, PyObject *args) {
+    const char *cmd;
+    if (!PyArg_ParseTuple(args, "s", &cmd)) {return NULL;}
+    int sts = pyobj(cmd);
+    return PyLong_FromLong(sts);
+}
+
+static PyMethodDef example_methods[] = {
+    {"spam", spam, METH_VARARGS},
+    {"adder", add, METH_VARARGS}
+};
+
+static struct PyModuleDef example_definition = {
+    PyModuleDef_HEAD_INIT,
+    "example",
+    "test",
+    -1,
+    example_methods
+};
+
+PyMODINIT_FUNC PyInit_example(void) {
+  Py_Initialize();
+  PyObject *m = PyModule_Create(&example_definition);
+  return m;
+}
+
+
+int main() {
+   return 0;
+}
+
